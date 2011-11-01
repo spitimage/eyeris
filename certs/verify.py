cd33492d38c79f0226c49a818736797b088eb864
spitimage <aaron@blooco.com>
2011-11-01 15:25:47 -0600

    Read certs from string

--- certs/verify.py
+++ certs/verify.py
@@ -1,7 +1,9 @@
-from M2Crypto.X509 import load_cert
+from M2Crypto.X509 import load_cert, load_cert_string
 import base64
 
 # Note: This doesn't seem to work with DSA
+# You can also load certs from a string with load_cert_string
+
 
 def binary():
 	f = open('signature.txt')
@@ -42,6 +44,31 @@ def dobase64():
 
 	print 'verification result: ', result
 
-dobase64()
+def dobase64Str():
+	f = open('signature.txt')
+
+	b64 = f.read()
+
+	signature = base64.b64decode(b64)	
+
+	cf = open('new_cert.x509')
+
+	certString = cf.read()
+
+	c = load_cert_string(certString)
+
+	k = c.get_pubkey()
+
+	k.verify_init()
+
+	data = 'Jesus is Lord'
+
+	k.verify_update(data)
+
+	result = k.verify_final(signature)
+
+	print 'verification result: ', result
+
+dobase64Str()
 
 
